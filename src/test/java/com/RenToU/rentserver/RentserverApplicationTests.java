5538ce670718e52c0bf1e0222bd8407cb7bc8f3b
akaster99 <idavid29@naver.com>
2022-07-03 14:42:04 +0900

    first commit

---
+++ src/test/java/com/RenToU/rentserver/RentserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RenToU.rentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
