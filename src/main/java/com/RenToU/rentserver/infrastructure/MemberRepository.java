0b5bf047d92523640a83797037770460e04fd449
akaster99 <idavid29@naver.com>
2022-07-05 23:05:27 +0900

    feat: define JPAMemberRepsitory

--- src/main/java/com/RenToU/rentserver/infrastructure/MemberRepository.java
+++ src/main/java/com/RenToU/rentserver/infrastructure/MemberRepository.java
@@ -1,4 +1,18 @@
 package com.RenToU.rentserver.infrastructure;
 
+import com.RenToU.rentserver.domain.Member;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberRepository {
+    List<Member> findAll();
+
+    Optional<Member> findById(Long id);
+
+    Member save(Member member);
+
+    void delete(Member member);
+
 }
