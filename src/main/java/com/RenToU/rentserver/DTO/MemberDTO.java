b16e88dd4d60c7a0c5461b7be0b54856fd76c8d5
akaster99 <idavid29@naver.com>
2022-07-10 03:06:44 +0900

    feat: add name at member

---
+++ src/main/java/com/RenToU/rentserver/DTO/MemberDTO.java
@@ -0,0 +1,45 @@
+package com.RenToU.rentserver.DTO;
+
+import com.RenToU.rentserver.domain.ClubMember;
+import com.github.dozermapper.core.Mapping;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+    private Long id;
+    @NotBlank
+    @Mapping("email")
+    private String email;
+    @NotBlank
+    @Mapping("name")
+    private String name;
+    @NotBlank
+    @Mapping("phoneNumber")
+    private String phoneNumber;
+    @NotBlank
+    @Mapping("studentId")
+    private String studentId;
+    @NotBlank
+    @Mapping("major")
+    private String major;
+
+    @OneToMany(mappedBy = "member")
+    private List<ClubMember> clubList = new ArrayList<>();
+
+}
