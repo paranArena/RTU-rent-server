0e788ef2fe11e043c8dbebe61023f5e32d4eafd5
akaster99 <idavid29@naver.com>
2022-08-31 00:09:05 +0900

    fix: requestClubJoin 2번 저장 되는 버그 해

---
+++ src/main/java/com/RenToU/rentserver/infrastructure/ClubMemberRepository.java
@@ -0,0 +1,11 @@
+package com.RenToU.rentserver.infrastructure;
+
+import com.RenToU.rentserver.domain.Club;
+import com.RenToU.rentserver.domain.ClubMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClubMemberRepository extends JpaRepository<ClubMember,Long> {
+
+}
