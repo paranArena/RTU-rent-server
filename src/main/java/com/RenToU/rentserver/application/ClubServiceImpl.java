81e4c59ae333bfdac4a1a274efa67ea1c140de0a
akaster99 <idavid29@naver.com>
2022-09-01 13:35:19 +0900

    ClubService getMyClubRequests 테스트 작성

--- src/main/java/com/RenToU/rentserver/application/ClubServiceImpl.java
+++ src/main/java/com/RenToU/rentserver/application/ClubServiceImpl.java
@@ -71,8 +71,13 @@ public class ClubServiceImpl implements ClubService{
         Member member = findMember(memberId);
         Club club = findClub(clubId);
         validateCanJoin(club, member);
+//        왠지 모르겠는데 print를 빼면 정상 작동을 안합니다... 추후에 해결해야 할 부분.
+        member.getClubList().forEach(cm->{
+            System.out.println(cm.toString());
+        });
         ClubMember clubMember = ClubMember.createClubMember(club, member, ClubRole.WAIT);
         clubMemberRepository.save(clubMember);
+        System.out.println("size"+member.getClubList().size()+",clubId "+club.getId() );
     }
 
     @Override
@@ -113,12 +118,8 @@ public class ClubServiceImpl implements ClubService{
     @Override
     public List<Club> getMyClubRequests(long memberId){
         Member member = findMember(memberId);
-        List<ClubMember> requests = member.getClubList().stream().filter(cm->{
-            return cm.getRole() == ClubRole.WAIT;
-        }).collect(Collectors.toList());
-        return requests.stream().map(cm -> {
-            return cm.getClub();
-        }).collect(Collectors.toList());
+        List<ClubMember> requests = member.getClubList().stream().filter(cm->cm.getRole().equals(ClubRole.WAIT)).collect(Collectors.toList());
+        return requests.stream().map(cm -> cm.getClub()).collect(Collectors.toList());
     }
 
 
