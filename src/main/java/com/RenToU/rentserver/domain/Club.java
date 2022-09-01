d6412a3806ba10b307406da5e384db44694feed9
akaster99 <idavid29@naver.com>
2022-09-01 13:35:19 +0900

    ClubService getMyClubRequests 테스트 작성

--- src/main/java/com/RenToU/rentserver/domain/Club.java
+++ src/main/java/com/RenToU/rentserver/domain/Club.java
@@ -10,6 +10,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
@@ -26,7 +27,7 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class Club extends BaseTimeEntity{
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "club_id")
     private Long id;
 
