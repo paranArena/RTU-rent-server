50da4852180fa698dcda0d37115a4f5637ee3597
akaster99 <idavid29@naver.com>
2022-09-06 14:51:43 +0900

    scedulerService 수정

--- src/main/java/com/RenToU/rentserver/application/ScheduleService.java
+++ src/main/java/com/RenToU/rentserver/application/ScheduleService.java
@@ -3,8 +3,10 @@ package com.RenToU.rentserver.application;
 import com.RenToU.rentserver.domain.Item;
 import com.RenToU.rentserver.domain.Member;
 import com.RenToU.rentserver.domain.Rental;
+import com.RenToU.rentserver.domain.RentalHistory;
 import com.RenToU.rentserver.domain.RentalStatus;
 import com.RenToU.rentserver.infrastructure.MemberRepository;
+import com.RenToU.rentserver.infrastructure.RentalHistoryRepository;
 import com.RenToU.rentserver.infrastructure.RentalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,7 @@ import java.util.List;
 @Service
 public class ScheduleService {
     private final RentalRepository rentalRepository;
+    private final RentalHistoryRepository rentalHistoryRepository;
 
     @Transactional
     public void checkExpiredRentalWait() {
@@ -25,6 +28,9 @@ public class ScheduleService {
         List<Rental> rentals = rentalRepository.findAllByRentalStatus(RentalStatus.WAIT);
         rentals.stream().forEach(rental -> {
             if (rental.getRentDate().plusMinutes(10).isBefore(LocalDateTime.now())) {
+                rental.cancel();
+                RentalHistory rentalHistory = RentalHistory.RentalToHistory(rental);
+                rentalHistoryRepository.save(rentalHistory);
                 rentalRepository.deleteById(rental.getId());
             }
         });
