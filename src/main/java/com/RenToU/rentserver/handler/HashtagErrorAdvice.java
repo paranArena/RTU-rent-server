5f0ad1131b7766708613e793f37a64ee8383c5ba
Haechan Lee <dlgockssla@gmail.com>
2022-09-08 02:13:29 +0900

    move to handler

---
+++ src/main/java/com/RenToU/rentserver/handler/HashtagErrorAdvice.java
@@ -0,0 +1,19 @@
+package com.RenToU.rentserver.handler;
+
+import com.RenToU.rentserver.dto.ErrorResponse;
+import com.RenToU.rentserver.exceptions.HashtagNotFoundException;
+import com.RenToU.rentserver.exceptions.club.ClubNotFoundException;
+import com.RenToU.rentserver.exceptions.club.CannotJoinClubException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class HashtagErrorAdvice {
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(HashtagNotFoundException.class)
+    public ErrorResponse HashtagNotFound(HashtagNotFoundException ex) {
+        return new ErrorResponse(ex.getMessage());
+    }
+}
