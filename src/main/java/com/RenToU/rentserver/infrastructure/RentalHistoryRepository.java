7cb1e2af7b3baa0d2553ca1a6b15a4b5150e05e5
Haechan Lee <dlgockssla@gmail.com>
2022-09-10 02:14:11 +0900

    resolve warning

--- src/main/java/com/RenToU/rentserver/infrastructure/RentalHistoryRepository.java
+++ src/main/java/com/RenToU/rentserver/infrastructure/RentalHistoryRepository.java
@@ -2,7 +2,6 @@ package com.RenToU.rentserver.infrastructure;
 
 import com.RenToU.rentserver.domain.Item;
 import com.RenToU.rentserver.domain.RentalHistory;
-import com.RenToU.rentserver.domain.RentalStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
