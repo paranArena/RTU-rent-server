a2cfd3a7d3e0fc9050f510cac79296d9d2fc8459
Haechan Lee <dlgockssla@gmail.com>
2022-09-11 16:49:37 +0900

    refactor errcode

--- src/main/java/com/RenToU/rentserver/exceptions/ClubErrorCode.java
+++ src/main/java/com/RenToU/rentserver/exceptions/ClubErrorCode.java
@@ -9,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ClubErrorCode implements ErrorCode {
 
-    NOT_AJOU_EMAIL(HttpStatus.BAD_REQUEST, "아주대학교 이메일이 아닙니다."),
-    WRONG_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "인증 코드가 올바르지 않습니다."),
     DUP_CLUB_NAME(HttpStatus.BAD_REQUEST, "이미 존재하는 클럽 이름입니다."),
     CANT_REQUEST_JOIN(HttpStatus.BAD_REQUEST, "대상이 이미 가입하였거나, 가입 신청 상태입니다."),
     CANT_GRANT_USER(HttpStatus.BAD_REQUEST, "유저로 권한 변경이 불가한 대상입니다."),
@@ -27,8 +25,8 @@ public enum ClubErrorCode implements ErrorCode {
     NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 클럽에서 해당 공지사항을 찾을 수 없습니다."),
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 클럽에서 해당 물품을 찾을 수 없습니다."),
     ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 클럽에서 해당 아이템을 찾을 수 없습니다."),
-    NO_MATCHING_ROLE(HttpStatus.FORBIDDEN,"해당 작업을 수행하기 위한 역할이 없습니다." ),
-    CLUB_OWNER_CANT_QUIT(HttpStatus.FORBIDDEN,"클럽장은 회원 탈퇴가 불가능합니다.");
+    NO_MATCHING_ROLE(HttpStatus.FORBIDDEN, "해당 작업을 수행하기 위한 역할이 없습니다."),
+    CLUB_OWNER_CANT_QUIT(HttpStatus.FORBIDDEN, "클럽장은 회원 탈퇴가 불가능합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
