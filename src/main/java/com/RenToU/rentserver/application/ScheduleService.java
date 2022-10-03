273c21108deb7998cb0503ee81db0e34115c4fe6
akaster99 <idavid29@naver.com>
2022-10-04 02:47:59 +0900

    ren2u 전체 가입 메소드

--- src/main/java/com/RenToU/rentserver/application/ScheduleService.java
+++ src/main/java/com/RenToU/rentserver/application/ScheduleService.java
@@ -1,8 +1,15 @@
 package com.RenToU.rentserver.application;
 
+import com.RenToU.rentserver.domain.Club;
+import com.RenToU.rentserver.domain.ClubMember;
+import com.RenToU.rentserver.domain.ClubRole;
+import com.RenToU.rentserver.domain.Member;
 import com.RenToU.rentserver.domain.Rental;
 import com.RenToU.rentserver.domain.RentalHistory;
 import com.RenToU.rentserver.domain.RentalStatus;
+import com.RenToU.rentserver.infrastructure.ClubMemberRepository;
+import com.RenToU.rentserver.infrastructure.ClubRepository;
+import com.RenToU.rentserver.infrastructure.MemberRepository;
 import com.RenToU.rentserver.infrastructure.RentalHistoryRepository;
 import com.RenToU.rentserver.infrastructure.RentalRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +18,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -18,6 +27,9 @@ import java.util.List;
 public class ScheduleService {
     private final RentalRepository rentalRepository;
     private final RentalHistoryRepository rentalHistoryRepository;
+    private final ClubMemberRepository clubMemberRepository;
+    private final MemberRepository memberRepository;
+    private final ClubRepository clubRepository;
 
     @Transactional
     public void checkExpiredRentalWait() {
@@ -32,6 +44,21 @@ public class ScheduleService {
                     rentalRepository.deleteById(rental.getId());
                 }
             });
+        }
+    }
+    @Transactional
+    public void joinRen2UAll() {
+        Club club = clubRepository.findById(19L).get();
+        List<Long> cmId = clubMemberRepository.findAllByClubId(19L)
+                .stream().map(cm->cm.getMember().getId()).collect(Collectors.toList());
+        for(Long i = 1L; i <= 200L; i++){
+            if(cmId.contains(i)){
+                continue;
+            }
+            Optional<Member> member = memberRepository.findById(i);
+            if(member.isPresent()){
+                clubMemberRepository.save(ClubMember.createClubMember(club,member.get(), ClubRole.USER));
+            }
         }
     }
 }
--- src/main/java/com/RenToU/rentserver/controller/ScheduleController.java
+++ src/main/java/com/RenToU/rentserver/controller/ScheduleController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +28,13 @@ public class ScheduleController {
         }
         scheduleService.checkExpiredRentalWait();
         return ResponseEntity.ok(ResponseDto.res(StatusCode.OK, ResponseMessage.CHECK_EXPIRED_RENTAL_WAIT));
+    }
+    @PostMapping("/join/ren2u/all")
+    public ResponseEntity<?> joinRen2UAll(@RequestHeader("secretKey") String secretKey) {
+        if (!secretKey.equals("ajouparan2022-2")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        scheduleService.joinRen2UAll();
+        return ResponseEntity.ok(ResponseDto.res(StatusCode.OK, "렌트유 가입 성공"));
     }
 }
