a004f6b6ddc822d512704cee30e100c16026f741
akaster99 <idavid29@naver.com>
2022-10-04 02:47:59 +0900

    ren2u 전체 가입 메소드

--- src/main/java/com/RenToU/rentserver/infrastructure/ClubMemberRepository.java
+++ src/main/java/com/RenToU/rentserver/infrastructure/ClubMemberRepository.java
@@ -3,6 +3,10 @@ package com.RenToU.rentserver.infrastructure;
 import com.RenToU.rentserver.domain.ClubMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+
+    List<ClubMember> findAllByClubId(Long clubId);
 
 }
