fb994752c058394a6454383e3e27ac4fe53e9dae
Haechan Lee <dlgockssla@gmail.com>
2022-11-28 00:34:41 +0900

    feat: event driven async fcm

---
+++ src/main/java/com/RenToU/rentserver/event/CreateNotificationEventListener.java
@@ -0,0 +1,41 @@
+package com.RenToU.rentserver.event;
+
+import com.RenToU.rentserver.application.FirebaseCloudMessageService;
+import com.RenToU.rentserver.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Component
+@Async("matching")
+@Transactional
+@RequiredArgsConstructor
+public class CreateNotificationEventListener {
+
+    private final FirebaseCloudMessageService firebaseCloudMessageService;
+
+    @EventListener
+    public void handleMatchingCompleteEvent(CreateNotificationEvent createNotificationEvent) {
+
+        // 알림 보낼 멤버 목록
+        List<Member> memberList = createNotificationEvent.getMemberList();
+
+        // fcmToken 뽑기
+        List<String> fcmTokenList = memberList
+                .stream()
+                .map(Member::getFcmToken)
+                .filter(fcmToken -> fcmToken != null && !fcmToken.isBlank()).collect(toList());
+
+        // 알림 보내기
+        if (fcmTokenList.size() != 0) {
+            firebaseCloudMessageService.sendByTokenList(fcmTokenList);
+        }
+
+    }
+}
