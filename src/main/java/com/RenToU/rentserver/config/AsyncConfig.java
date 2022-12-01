4d5a093571526db6635a4ffca811fbd1b90174e5
Haechan Lee <dlgockssla@gmail.com>
2022-12-02 02:57:43 +0900

    feat: send push message when rental expired at tomorrow

--- src/main/java/com/RenToU/rentserver/config/AsyncConfig.java
+++ src/main/java/com/RenToU/rentserver/config/AsyncConfig.java
@@ -14,11 +14,25 @@ import java.util.concurrent.Executor;
 public class AsyncConfig {
 
     @Bean(name = "createNotification")
-    public Executor threadPoolExecutor() {
+    public Executor createNotificationThreadPoolExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         int processors = Runtime.getRuntime().availableProcessors();
         log.info("processors count {}", processors);
         executor.setThreadNamePrefix("CreateNotificationAsync-"); // thread 이름 설정
+        executor.setCorePoolSize(processors); // 기본 스레드 수
+        executor.setMaxPoolSize(processors * 2); // 최대 스레드 개수
+        executor.setQueueCapacity(50); // 최대 큐 수
+        executor.setKeepAliveSeconds(60); // maxpoolsize로 인해 덤으로 더 돌아다니는 튜브는 60초 후에 수거해서 정리
+        executor.initialize(); // 초기화후 반환
+        return executor;
+    }
+
+    @Bean(name = "rentalExpirationRemind")
+    public Executor rentalExpirationRemindThreadPoolExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        int processors = Runtime.getRuntime().availableProcessors();
+        log.info("processors count {}", processors);
+        executor.setThreadNamePrefix("RentalExpirationRemindAsync-"); // thread 이름 설정
         executor.setCorePoolSize(processors); // 기본 스레드 수
         executor.setMaxPoolSize(processors * 2); // 최대 스레드 개수
         executor.setQueueCapacity(50); // 최대 큐 수
